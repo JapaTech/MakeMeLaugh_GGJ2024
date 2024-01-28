48c25137a23293783ee5a466d284241aa3215ffa
Jonathan Iha <58711422+JapaTech@users.noreply.github.com>
2024-01-27 22:47:56 -0300

    FMOD banks

---
+++ MakeMeLaugh_GGJ2024/Assets/Scripts/Audio/FMODAudioManager.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+using FMODUnity;
+using FMOD.Studio;
+
+public class FMODAudioManager : MonoBehaviour
+{
+    public static FMODAudioManager Instance;
+
+    private void Awake()
+    {
+        if (Instance != null && Instance != this)
+        {
+            Destroy(gameObject);
+        }
+        else
+        {
+            Instance = this;
+        }
+
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
